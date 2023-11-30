e91527e8ba99a59364d653aa65b47e0f8f2fa8d9
Antonino <pagana899@gmail.com>
2023-11-30 11:35:02 +0100

    Aggiunto dati

--- Team.java
+++ Team.java
@@ -1,6 +1,7 @@
 public class Team {
     public static void main(String[] args) {
 
+    System.out.println("Antonino, Pagana, 20, Catania, Netflix, Sushi, FIFA, Now You See Me, ' ', ' ' ");
 
     }
 }
