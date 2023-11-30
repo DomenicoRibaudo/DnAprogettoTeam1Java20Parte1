47caf7ed99036416a426425b16efdfb8dcabc628
Andriy Bardak <ant.barda@gmail.com>
2023-11-30 11:34:47 +0100

    aggiunto i miei dati

--- Team.java
+++ Team.java
@@ -1,6 +1,9 @@
 public class Team {
     public static void main(String[] args) {
 
+        System.out.println("Andriy, Bardakhivskyy, 25, Montecchio Emilia, trekking, pasta, StrongHold +" +
+                "Pulp Fiction, Un sacchetto di biglie, '' ");
+
 
     }
 }
