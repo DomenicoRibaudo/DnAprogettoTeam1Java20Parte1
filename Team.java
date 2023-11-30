d41cec8a40826c5af22af3972e87eb9d53c86ba7
Domenico <domenicoribaudo06@gmail.com>
2023-11-30 11:48:27 +0100

    aggiunte modifiche a team.java

--- Team.java
+++ Team.java
@@ -1,6 +1,6 @@
 public class Team {
     public static void main(String[] args) {
-
-
+        System.out.println("Domenico, Ribaudo, 23, Palermo, Gym, pizza, Marvel Spiderman, American Psycho," +
+                "Harry Potter, Sid ");
     }
 }
