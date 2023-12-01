0c90a6050c9378ac7ba64581f0f3ee8eec6709d1
Domenico <domenicoribaudo06@gmail.com>
2023-12-01 10:44:43 +0100

    aggiornato l'inserimento di entrambi i file

--- Team.java
+++ Team.java
@@ -1,5 +1,7 @@
 public class Team {
     public static void main(String[] args) {
-
+        Domenico domenico1 = new Domenico("Domenico", "Ribaudo", 23, "Palermo", "Gym",
+                "Pizza", "Marvel Spiderman", "AmericanPsycho", "Harry Potter", "Sid");
+        System.out.println(domenico1);
     }
 }
