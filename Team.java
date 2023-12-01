88acbc7a971a3ea2dbd744693def8d8210c3e14b
Antonino <pagana899@gmail.com>
2023-12-01 10:37:46 +0100

    Inserimento e stampa eseguiti

--- Team.java
+++ Team.java
@@ -1,7 +1,10 @@
 public class Team {
     public static void main(String[] args) {
 
-    System.out.println("Antonino, Pagana, 20, Catania, Netflix, Sushi, FIFA, Now You See Me, ' ', ' ' ");
+    Antonino antoninopagana = new Antonino("Antonino", "Pagana", 20, "Catania", "Netflix", "Sushi", "FIFA",
+            "Now You See Me", "' '", "' '");
+
+    System.out.println(antoninopagana);
 
     }
 }
